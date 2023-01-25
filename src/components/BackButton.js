8d31335a2698686c84e0b3787e7c45ba66a5547c
Thomas Thirion <thirionthomas68100@gmail.com>
2023-01-25 16:57:57 +0100

    finalisation style

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -6,6 +6,7 @@ export const BackButton = () => {
       <div className="grid justify-items-stretch">
         <div className="justify-self-end m-10">
           <button
+            className="evenement"
             onClick={() => {
               window.history.back();
             }}
